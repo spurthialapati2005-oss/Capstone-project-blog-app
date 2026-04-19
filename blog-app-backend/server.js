a7900a652989bab36e21fa76ba95c1427a216cbb
spurthialapati2005-oss <spurthialapati2005@gmail.com>
2026-04-19 11:34:19 +0530

    Update server.js

--- blog-app-backend/server.js
+++ blog-app-backend/server.js
@@ -15,7 +15,7 @@ config() // process.env
 const app = exp()   
 
 //use cors middleware
-app.use(cors({origin:["capstone-project-blog-app.vercel.app"], credentials:true}));
+app.use(cors({origin:["https://capstone-project-blog-app.vercel.app"], credentials:true}));
 
 //add body parser middleware
 app.use(exp.json())
